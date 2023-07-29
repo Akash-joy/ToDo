d29499814a402a9bd4a3ea87252fe9d179a20f1d
akash_appscook <akash@appscook.com>
2023-07-29 16:30:06 +0530

    Arranged

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,37 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent {
-  newTask: string = '';
+   newTask:string ='';
+   currentDate: Date;
   tasks: string[] = [];
   editIndex: number | null = null;
-  editedTask: string = ''; // Variable to store the edited value
-
-  addTask() {
-    if (this.newTask.trim() !== '') {
+  editedTask: string = ''; 
+  constructor() {
+    this.currentDate = new Date(); 
+  }
+  addTask() {   
       const existingIndex = this.tasks.indexOf(this.newTask);
-
       if (existingIndex !== -1) {
         this.tasks[existingIndex] = this.newTask;
       } else {
         this.tasks.push(this.newTask);
       }
-
-      this.newTask = '';
-      this.editIndex = null; // Reset editIndex to avoid confusion when adding a new task
-    }
+      this.newTask ='';
+      this.editIndex = null; 
+    
   }
 
-  Edit(index: number) {
+  Edit(index: number) { 
     this.editIndex = index;
-    this.editedTask = this.tasks[index]; // Store the original task value to the editedTask variable
+    this.editedTask = this.tasks[index]; 
   }
 
   SubmitEdit(index: number) {
-    if (this.editedTask.trim() !== '') {
-      this.tasks[index] = this.editedTask; // Update the task value in the array
-    }
+    
+      this.tasks[index] = this.editedTask; 
+    
     this.editIndex = null;
-    this.editedTask = ''; // Clear the editedTask variable
+    this.editedTask = ''; 
   }
 
   Delete(index: number) {
