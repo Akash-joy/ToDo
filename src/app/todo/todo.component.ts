9192fb389207ed16da0e5d00ceea62a1f9513381
akash_appscook <akash@appscook.com>
2023-07-31 07:48:46 +0530

    Added date and time

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,45 +1,78 @@
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent {
-   newTask:string ='';
-   currentDate: Date;
-  tasks: string[] = [];
+  newTask: string = '';
+  currentDate: Date;
+  tasks: { taskName: string; taskDate: string; taskTime: string }[] = [];
   editIndex: number | null = null;
-  editedTask: string = ''; 
+  editedTask: string = '';
+  currentDateOnly: string = '';
+  time: string = '';
+
   constructor() {
-    this.currentDate = new Date(); 
-  }
-  addTask() {   
-      const existingIndex = this.tasks.indexOf(this.newTask);
-      if (existingIndex !== -1) {
-        this.tasks[existingIndex] = this.newTask;
-      } else {
-        this.tasks.push(this.newTask);
-      }
-      this.newTask ='';
-      this.editIndex = null; 
-    
+    this.currentDate = new Date();
+    this.currentDateOnly = this.getCurrentDateOnly();
+    this.time = this.getCurrentTime();
   }
 
-  Edit(index: number) { 
+  addTask() {
+    const trimmedTask = this.newTask.trim();
+    if (trimmedTask !== '') {
+      const existingIndex = this.tasks.findIndex((task) => task.taskName === trimmedTask);
+      if (existingIndex !== -1) {
+        this.tasks[existingIndex].taskName = trimmedTask;
+      } else {
+        this.tasks.push({
+          taskName: trimmedTask,
+          taskDate: this.currentDateOnly,
+          taskTime: this.time,
+        });
+      }
+
+      this.newTask = '';
+      this.editIndex = null;
+    }
+  }
+
+  Edit(index: number) {
     this.editIndex = index;
-    this.editedTask = this.tasks[index]; 
+    this.editedTask = this.tasks[index].taskName;
   }
 
   SubmitEdit(index: number) {
-    
-      this.tasks[index] = this.editedTask; 
-    
+    if (this.editedTask.trim() !== '') {
+      this.tasks[index].taskName = this.editedTask;
+      
+    }
     this.editIndex = null;
-    this.editedTask = ''; 
+    this.editedTask = '';
   }
 
   Delete(index: number) {
     this.tasks.splice(index, 1);
+  }
+
+  getCurrentDateOnly(): string {
+    const year = this.currentDate.getFullYear();
+    const month = this.addLeadingZero(this.currentDate.getMonth() + 1);
+    const day = this.addLeadingZero(this.currentDate.getDate());
+    return `${year}-${month}-${day}`;
+  }
+
+  addLeadingZero(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }
+
+  getCurrentTime() {
+    const hours = this.addLeadingZero(this.currentDate.getHours());
+    const minutes = this.addLeadingZero(this.currentDate.getMinutes());
+    const seconds = this.addLeadingZero(this.currentDate.getSeconds());
+    return `${hours}:${minutes}:${seconds}`;
   }
 }
